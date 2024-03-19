2772da2afbffc093789f414b1dca0b0258fb85c8
hubertdeveloper <87448072+hubertdeveloper@users.noreply.github.com>
2024-03-19 22:07:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 int main() {
     cout << "witaj swiecie";
+    return EXIT_SUCCESS;
 }
 */
 /*
@@ -11,8 +12,10 @@ int main() {
 #include <iostream>
 using namespace std;
 int main() {
-    for(int i{0};i<13;i++)
-        cout <<i+1<< ". witaj swiecie\n";
+    for(int i{0};i<13;i++){
+        cout <<"witaj swiecie\n";
+    }
+    return EXIT_SUCCESS;
 }
 */
 /*
@@ -20,12 +23,12 @@ int main() {
 #include <iostream>
 using namespace std;
 int Zwiekszacz(int liczba) {
-    liczba++;
-    return liczba;
+    return liczba+1;
 }
 int main() {
     int liczba{3};
     cout<<Zwiekszacz(liczba);
+    return EXIT_SUCCESS;
 }
 */
 /*
@@ -33,14 +36,14 @@ int main() {
 #include <iostream>
 using namespace std;
 int Zwiekszacz(int liczba) {
-    liczba++;
-    return liczba;
+    return liczba+1;
 }
 int main() {
     int liczba;
     cout <<"Podaj liczbe: ";
     cin >>liczba;
     cout <<"Liczba zwiekszona o jeden wynosi: "<<Zwiekszacz(liczba);
+    return EXIT_SUCCESS;
 }
 */
 /*
@@ -48,12 +51,13 @@ int main() {
 #include <iostream>
 using namespace std;
 int Zwiekszacz(int liczba) {
-    liczba++;
-    return liczba;
+    return liczba+1;
 }
 int main() {
-    for(int i{0};i<18;i++)
-        cout <<"Argument "<<i<<", zwiekszacz("<<i<<") = "<<Zwiekszacz(i)<<endl;
+    for(int i{0};i<18;i++){
+        cout <<"Argument "<<i<<", zwiekszacz("<<i<<") = "<<Zwiekszacz(i)<<"\n";
+    }
+    return EXIT_SUCCESS;
 }
 */
 /*
@@ -62,14 +66,14 @@ int main() {
 #include <cmath>
 using namespace std;
 double odleglosc(int x1, int y1, int z1, int x2, int y2, int z2) {
-    double d{sqrt(pow((x1-x2),2)+pow((y1-y2),2)+pow((z1-z2),2))};
-    return d;
+    return sqrt(pow((x1-x2),2)+pow((y1-y2),2)+pow((z1-z2),2));
 }
 int main() {
         int x1{1}, y1{2}, z1{0}, x2{3}, y2{7}, z2{8};
         cout<<"a = ("<<x1<<","<<y1<<","<<z1<<")\n";
         cout<<"b = ("<<x2<<","<<y2<<","<<z2<<")\n";
         cout<<"Odleglosc miedzy punktami wynosi: "<<odleglosc(x1,y1,z1,x2,y2,z2);
+        return EXIT_SUCCESS;
 }
 */
 /*
@@ -77,29 +81,22 @@ int main() {
 #include <iostream>
 using namespace std;
 int max(int a, int b, int c) {
-    if(a>=b) {
-        if (a>c)
-            return a;
-        else
+    if(a>b) {
+        if(a>c) {
             return c;
+        }
+        return b;
     }
-    else if(b>=a) {
-        if(b>c)
-            return b;
-        else
-            return c;
+    else if(b>c) {
+        return b;
     }
-    else if(c>=a) {
-        if(c>b)
-            return c;
-        else
-            return b;
-    }
+    else return c;
 }
 int main() {
     int a{7}, b{42}, c{32};
     cout<<"a="<<a<<" b="<<b<<" c="<<c;
     cout<<"\nNajwieksza liczba wynosi: "<<max(a,b,c);
+    return EXIT_SUCCESS;
 }
 */
 /*
@@ -108,27 +105,36 @@ int main() {
 using namespace std;
 
 int main() {
-    for (char i{97};i<123;i++)
+    for (char i{'a'};i<='z';i++) {
         cout<<"Znak: "<<i<<" Kod ASCII: "<<(int)i<<"\n";
+    }
+    return EXIT_SUCCESS;
 }
 */
-/*
+
 // zad9
 #include <iostream>
 using namespace std;
 
 void trojkat() {
-    for(int i{1};i<=9;i++) {
-        for(int j{1};j<=i;j++) {
-                cout <<i;
+    for(int i{9}; i >= 1; i--) {
+        for(int j{1}; j <= i; j++) {
+            cout << i;
         }
-        cout<<endl;
+        cout << "\n";
+    }
+    for(int i{1}; i <= 9; i++) {
+        for(int j{1}; j <= i; j++) {
+            cout << i;
+        }
+        cout << "\n";
     }
 }
 int main() {
     trojkat();
+    return EXIT_SUCCESS;
 }
-*/
+
 /*
 // zad10
 #include <iostream>
@@ -149,12 +155,11 @@ float maksimum(float tab[], int n) {
     return max;
 }
 int main() {
-    int n=5;
-    float tab[n]{5.2,6.4,7.2,5.1,1.1};
-    for(int i=0;i<n;i++)
-        cout<<tab[i]<<", ";
+    float tab[]{5.2,6.4,7.2,5.1,1.1};
+    int n=sizeof(tab)/sizeof(tab[0]);
     cout <<endl<<"Najmniejsza liczba z tablicy wynosi: "<<minimum(tab,n);
     cout <<endl<<"Najwieksza liczba z tablicy wynosi: "<<maksimum(tab,n);
+    return EXIT_SUCCESS;
 }
 */
 /*
@@ -257,35 +262,6 @@ int main() {
 }
 */
 /*
-//zad 13
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    double a,c;
-    double bi,di;
-    double wynik[2] = {0,0};
-
-    a=2;
-    bi=4;
-    c=5;
-    di=1;
-    // (2+2i) * (3+3i) = 6 + 6i + 6i + 6i^2= 6 - 6 + 12i = 12i
-    wynik[0] = a*c - bi*di*1.0;
-    wynik[1] = a*di + c*bi*1.0;
-
-    cout << "wynik: " << wynik[0] << " + " << wynik[1] <<"i\n";
-
-    wynik[0] = a*c - (bi*(-di));
-    wynik[1] = a*(-di) + c*bi;
-
-    cout << "wynik: " << wynik[0]/(c*c - (di*(-di))) << " + " << wynik[1]/(c*c - (di*(-di))) << "i";
-
-    return 0;
-}
-*/
-/*
 // zad14
 #include <iostream>
 #include <cmath>
@@ -327,4 +303,24 @@ int main() {
         cout<<"\nDruga data "<<dzien2<<"."<<miesiac2<<"."<<rok2<<"r. jest wczesniejsza";
 }
  */
+/*
 // zad 16
+#include <iostream>
+using namespace std;
+
+int minimum(int tab[], int n) {
+    int min=tab[0];
+    for(int i=0; i<n;i+=2) {
+        if(tab[i]<min) {
+            min=tab[i];
+        }
+    }
+    return min;
+}
+int main() {
+    int tab[]={12,2,4,-13,2,56,5,12,13,1,31,0,31,-12};
+    int n=sizeof(tab)/sizeof(tab[0]);
+    cout<<minimum(tab,n);
+    return EXIT_SUCCESS;
+}
+*/
